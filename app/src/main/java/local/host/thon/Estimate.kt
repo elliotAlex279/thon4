5f95312f1863a445ba5905ee30f6de1fb414d282
jeel279 <jeel4402@gmail.com>
2021-06-25 08:36:49 +0530

    New Update j

--- app/src/main/java/local/host/thon/Estimate.kt
+++ app/src/main/java/local/host/thon/Estimate.kt
@@ -3,9 +3,13 @@ package local.host.thon
 import android.os.Bundle
 import android.os.StrictMode
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class Estimate : Fragment() {
     override fun onCreateView(
@@ -17,6 +21,12 @@ class Estimate : Fragment() {
         StrictMode.setThreadPolicy(policy)
         super.onCreate(savedInstanceState)
         val view =  inflater.inflate(R.layout.estimate, container, false)
+        val btn1 = view.findViewById<MaterialButton>(R.id.cal_btn1)
+        val ans1 = view.findViewById<TextView>(R.id.cal1)
+        btn1.setOnClickListener {
+            val x = view.findViewById<TextInputEditText>(R.id.inp1)
+            ans1.text = (x.text.toString().toFloat() * 30F * 2F).toString()
+        }
         return view;
     }
 }
--- app/src/main/java/local/host/thon/sql.java
+++ app/src/main/java/local/host/thon/sql.java
@@ -1,4 +1,6 @@
 package local.host.thon;
+import android.os.StrictMode;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,6 +15,8 @@ public class sql {
         String url = "jdbc:mysql://WKTDxt18Em@remotemysql.com:3306/WKTDxt18Em";
         String user = "WKTDxt18Em";
         String pass = "2g87yWwn89";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         conn = DriverManager.getConnection(url, user, pass);
         return conn;
     }
